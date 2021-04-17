457ac04e73f33762df022c4f74ca81baf139619c
“Dmitrii <dimakon2020@gmail.com>
2021-04-17 13:21:31 +0700

    change 1_6_10_registration.py for test 1_6_11

--- 1_6_10_registration.py
+++ 1_6_10_registration.py
@@ -4,6 +4,7 @@ import time
 
 try:
     link = "http://suninjuly.github.io/registration1.html"
+    #link = "http://suninjuly.github.io/registration2.html"
     browser = webdriver.Chrome()
     browser.get(link)
 
